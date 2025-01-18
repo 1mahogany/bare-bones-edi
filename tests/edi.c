03a7b76f63ed4259615500d5433845250d86d659
Ian Wang <65928789+1mahogany@users.noreply.github.com>
2025-01-17 20:06:19 -0600

    build out tests for base-reader

---
+++ tests/edi.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include "csv.h"
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        
+        printf("missing input file\n");
+        return EXIT_FAILURE;
+        
+    } else {
+
+        EDIFile* edi = EDI_FILE_INIT(argv[1]);
+        csv_parse_file(edi,stdout);
+        EDI_FILE_FREE(edi);
+
+        return EXIT_SUCCESS;
+
+    }
+
+}
